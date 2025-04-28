32f6e5145c45e592332dafc8ba89446fd17d863f
findwannawhy <findwannawhy@gmail.com>
2025-04-29 00:45:44 +0300

    added second half of hw

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+
+export const spawnChildProcess = async (args) => {
+  const child = spawn("node", [path.join(__dirname, "./files/script"), args]);
+  child.stdout.pipe(process.stdout);
+  process.stdin.pipe(child.stdin);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("--argument1 privet");
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "node:fs"
+import fsPromises from "node:fs/promises"
+import path from "path";
+import { pipeline } from "node:stream/promises";
+import { createGunzip } from "node:zlib"
+
+const __dirname = import.meta.dirname;
+const pathFileSource = path.join(__dirname, "./files/archive.gz");
+const pathFileDesination = path.join(__dirname, "./files/fileToCompress.txt");
+
+const unzip = createGunzip()
+
 const decompress = async () => {
-    // Write your code here 
+    const file = (await fsPromises.open(pathFileSource)).createReadStream();
+    const destination = fs.createWriteStream(pathFileDesination);
+    await pipeline(file, unzip, destination)
 };
 
 await decompress();
